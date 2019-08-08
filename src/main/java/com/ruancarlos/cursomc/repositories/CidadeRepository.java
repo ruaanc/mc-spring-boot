b0357127926c52310ee357cf0ad339fa4862cfbc
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-08-08 20:27:48 -0300

    Estado e Cidade

---
+++ src/main/java/com/ruancarlos/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.ruancarlos.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ruancarlos.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+	
+}
