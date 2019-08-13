430df20e09b6cdf67c100391ab48d47f57a63c10
Ruan Carlos <ruan.carlos@dce.ufpb.br>
2019-08-13 13:33:20 -0300

    Criando os objetos Pedido e Pagamento, e fazendo o relacionamento um para um

--- src/main/java/com/ruancarlos/cursomc/domain/Cliente.java
+++ src/main/java/com/ruancarlos/cursomc/domain/Cliente.java
@@ -37,6 +37,8 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
 	
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente() {
 	}
 
@@ -111,6 +113,14 @@ public class Cliente implements Serializable {
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
+	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
